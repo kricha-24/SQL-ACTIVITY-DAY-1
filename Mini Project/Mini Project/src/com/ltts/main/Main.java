e01fbdf34391d654049f6cfc1e0475e2dfe0bd35
kricha-24 <78844495+kricha-24@users.noreply.github.com>
2021-03-09 14:46:45 +0530

    Update Main.java

--- Mini Project/Mini Project/src/com/ltts/main/Main.java
+++ Mini Project/Mini Project/src/com/ltts/main/Main.java
@@ -2,11 +2,11 @@ package com.ltts.main;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import com.ltts.Dao.PlayerDao;
-import com.ltts.Dao.TeamDao;
+import com.ltts.dao.PlayerDAO;
+import com.ltts.dao.TeamDAO;
 import com.ltts.model.Player;
 import com.ltts.model.Team;
 
@@ -19,28 +19,18 @@ BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 Player t= new Player();
 Team t1 = new Team();
 
-System.out.println("Choose : ");
-
 System.out.println("1.Insert into table player : ");
-
 System.out.println("2. Search Player by PlayerId");
+System.out.println("3. Update the table Player/Team");
+System.out.println("4. View All the players");
+System.out.println("5. Delete Player");
+System.out.println("6.Insert into table team : ");
 
-System.out.println("3. Search Player by PlayerName");
-
-System.out.println("4. Update the table Player/Team");
-
-System.out.println("5. View All the players");
-
-System.out.println("6. View All the players based on Team");
-
-System.out.println("7. View All the players based on Team and Skill");
-
-System.out.println("8. Delete Player");
 
 
 int id =Integer.parseInt(br.readLine());
-PlayerDao td=new PlayerDao();
-TeamDao ttd=new TeamDao();
+PlayerDAO td=new PlayerDAO();
+TeamDAO ttd=new TeamDAO();
 
 
 if(id==1){
@@ -71,17 +61,13 @@ if(id==1){
 	System.out.println("Enter Player Nationality");
 	     String nnnname=br.readLine();
 	     System.out.println("Enter Player PowerRating");
-	     int pppppno=Integer.parseInt(br.readLine());
-
-	//team entry
-	System.out.println("Team Details: ");  
-	    
+	     double pppppno=Double.parseDouble(br.readLine());
 	  //  boolean b =ttd.createTeams();
-	    
+	    System.out.println("Enter Team ID: ");
+	    int tno=Integer.parseInt(br.readLine());
 	    ttd.displayTeams();
-	System.out.println("Enter Team Details: ");
-	System.out.println("Enter Team ID: ");
-	     int tno=Integer.parseInt(br.readLine());
+	     
+	
 	//calling player objects
 	      t= new Player(pno,name,ppno,nname,pppno,nnname,ppppno,nnnname,pppppno,tno);
 
@@ -111,159 +97,107 @@ if(id==2)
 
 if(id==3)		  
 {		
-System.out.println("Enter player playerName: ");
-String pname = br.readLine();
-t= new Player(0, pname, null, null, 0, 0, 0, null,0, 0);
-
- 
-td.searchPlayerbyname(t);
-
-}
-
-	if(id==4)
-	{
+	System.out.println("Choose 1/2 ");
+	System.out.println("1.Player Update");
+	System.out.println("2.Team Update");
+	int y=Integer.parseInt(br.readLine());
+	if(y==1){
 		System.out.println("Choose 1/2 ");
-		System.out.println("1.Player Update");
-		System.out.println("2.Team Update");
-		int y=Integer.parseInt(br.readLine());
-		if(y==1){
-			System.out.println("Choose 1/2 ");
-			System.out.println("1.Player Update runs");
-			System.out.println("2.Player Update wkts");
-			
-			int z=Integer.parseInt(br.readLine());
-			if(z==1)
-			{	
+		System.out.println("1.Player Update runs");
+		System.out.println("2.Player Update wkts");
+		
+		int z=Integer.parseInt(br.readLine());
+		if(z==1)
+		{	
 
-				  System.out.println("Enter player id: ");
+			  System.out.println("Enter player id: ");
+			  int pid = Integer.parseInt(br.readLine());
+			  
+			  System.out.println("Enter player runs : ");
+			  
+			  int pruns = Integer.parseInt(br.readLine());
+			  
+			  t= new Player(pid, null, null, null, 0, pruns, 0, null,0, 0);
+			  
+			   
+			  td.updatePRuns(t);
+		
+			
+		}
+		if(z==2)
+		{
+			System.out.println("Enter player id: ");
+			 int pid = Integer.parseInt(br.readLine());
+			  
+			 System.out.println("Enter player wickets : ");
+			  
+			  int pwiks = Integer.parseInt(br.readLine());
+			  
+			  t= new Player(pid, null, null, null, 0, 0, pwiks, null,0, 0);
+			  
+			   
+			  td.updatePWickets(t);
+		
+			
+		}
+	}
+		if(y==2)
+		{
+			System.out.println("Choose 1/2 ");
+			System.out.println("1.Team Update owner");
+			System.out.println("2.Team Update coach");
+			
+			int x=Integer.parseInt(br.readLine());
+			if(x==1){	
+
+				  System.out.println("Enter Team id: ");
 				  int pid = Integer.parseInt(br.readLine());
 				  
-				  System.out.println("Enter player runs : ");
+				  System.out.println("Enter Team owner name : ");
 				  
-				  int pruns = Integer.parseInt(br.readLine());
+				  String towner = br.readLine();
 				  
-				  t= new Player(pid, null, null, null, 0, pruns, 0, null,0, 0);
+				  t1= new Team(pid, null,towner, null,0);
 				  
 				   
-				  td.updatePRuns(t);
+				  ttd.updateTOwner(t1);
 			
 				
 			}
-			if(z==2)
+			if(x==2)
 			{
-				System.out.println("Enter player id: ");
+				System.out.println("Enter Team id: ");
 				 int pid = Integer.parseInt(br.readLine());
 				  
-				 System.out.println("Enter player wickets : ");
+				 System.out.println("Enter Team Coach : ");
 				  
-				  int pwiks = Integer.parseInt(br.readLine());
+				  String  tcoach = br.readLine();
 				  
-				  t= new Player(pid, null, null, null, 0, 0, pwiks, null,0, 0);
+				  t1= new Team(pid, null,null, tcoach,0);
 				  
 				   
-				  td.updatePWickets(t);
+				  ttd.updateTCoach(t1);
 			
 				
 			}
-		}
-			if(y==2)
-			{
-				System.out.println("Choose 1/2 ");
-				System.out.println("1.Team Update owner");
-				System.out.println("2.Team Update coach");
-				
-				int x=Integer.parseInt(br.readLine());
-				if(x==1){	
 
-					  System.out.println("Enter Team id: ");
-					  int pid = Integer.parseInt(br.readLine());
-					  
-					  System.out.println("Enter Team owner name : ");
-					  
-					  String towner = br.readLine();
-					  
-					  t1= new Team(pid, null,towner, null,0);
-					  
-					   
-					  ttd.updateTOwner(t1);
-				
-					
-				}
-				if(x==2)
-				{
-					System.out.println("Enter Team id: ");
-					 int pid = Integer.parseInt(br.readLine());
-					  
-					 System.out.println("Enter Team Coach : ");
-					  
-					  String  tcoach = br.readLine();
-					  
-					  t1= new Team(pid, null,null, tcoach,0);
-					  
-					   
-					  ttd.updateTCoach(t1);
-				
-					
-				}
-
-			}
-			
 		}
-	if(id==5)
+}
+
+	
+	
+	if(id==4)
 	{
+	
 		t= new Player(0, null, null, null, 0, 0, 0, null,0, 0);
 		  
 		   
 		  td.displayallPlayer(t);
-	
-	}
-
-	if(id==6)
-	{
-		t= new Player(0, null, null, null, 0, 0, 0, null,0, 0);
-		  
-		// t1= new Team(0, tname,null, null,0);  
-		
-		
-		System.out.println("View All the players based on Team");
-		
-		
-		System.out.println("Enter team name");
-		
-		String  tname = br.readLine();
-		t1= new Team(0, tname,null, null,0);  
-		
-		
-		td.displayPlayeronTeam(t,t1);
-	
-	}
-	
-	if(id==7)
-	{
-	
-		System.out.println("View All the players based on Team and skill");
-		
-		
-		System.out.println("Enter team name");
-		
-		String  tname = br.readLine();
 			
-		System.out.println("Enter player skill");
-		
-		String  pskill = br.readLine();
-		
-		
-		t=  new Player(0, null, null, pskill, 0, 0, 0, null,0, 0);
-		
-		t1= new Team(0, tname,null, null,0);  
-		
-		
-		td.displayPlayeronTeamandskill(t,t1);
-	
-	}
-	if(id==8)
+		}
+	if(id==5)
 	{
-		System.out.println("Enter player name to be deleted");
+System.out.println("Enter player name to be deleted");
 		
 		String  ppname = br.readLine();
 		
@@ -271,9 +205,7 @@ td.searchPlayerbyname(t);
 		  
 		   
 		  td.deletePlayer(t);
-	
 	}
-
 	}
 		
 }
